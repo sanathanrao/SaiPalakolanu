431afa51ca5bd2e2bbf43c76d373caaedef16022
unknown <{sanathan6522@gmail.com}>
2024-07-29 19:45:22 +0530

    first commit

--- send_email.php
+++ send_email.php
@@ -1,24 +1,42 @@
 <?php
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\Exception;
+
+require 'vendor/autoload.php';
+
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $name = htmlspecialchars($_POST['name']);
+    $phone = htmlspecialchars($_POST['phone']);
     $email = htmlspecialchars($_POST['email']);
-    $message = htmlspecialchars($_POST['message']);
+    $subject = htmlspecialchars($_POST['subject']);
     
-    $to = 'sanathan6522@gmail.com'; // Replace with your email address
-    $subject = 'New Form Submission';
-    $headers = "From: $email\r\n";
-    $headers .= "Reply-To: $email\r\n";
-    $headers .= "Content-Type: text/plain; charset=UTF-8\r\n";
-    
-    $emailBody = "Name: $name\nEmail: $email\n\nMessage:\n$message";
+    $mail = new PHPMailer(true);
 
-    if (mail($to, $subject, $emailBody, $headers)) {
-        echo 'Success';
-    } else {
-        echo 'Error';
+    try {
+        //Server settings
+        $mail->isSMTP();
+        $mail->Host       = 'smtp.gmail.com';  // SMTP server
+        $mail->SMTPAuth   = true;                // Enable SMTP authentication
+        $mail->Username   = 'sanathan6522@gmail.com';  // SMTP username
+        $mail->Password   = 'vxqxseqoankrreri';     // SMTP password
+        $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;  // Enable TLS encryption
+        $mail->Port       = 587;                 // TCP port
+
+        //Recipients
+        $mail->setFrom('sanathan6522@gmail.com', 'Mailer');
+        $mail->addAddress('sai@digicarotene.com', 'Recipient Name');  // Add a recipient
+
+        //Content
+        $mail->isHTML(true);  // Set email format to HTML
+        $mail->Subject = 'New Contact Form Submission';
+        $mail->Body    = "Name: $name<br>Phone: $phone<br>Email: $email<br>Subject: $subject<br>";
+        $mail->AltBody = "Name: $name\nPhone: $phone\nEmail: $email\nSubject: $subject";
+
+        $mail->send();
+        http_response_code(200);
+    } catch (Exception $e) {
+        error_log("Message could not be sent. Mailer Error: {$mail->ErrorInfo}");
+        http_response_code(500);
     }
-} else {
-    http_response_code(405);
-    echo 'Method Not Allowed';
 }
 ?>
